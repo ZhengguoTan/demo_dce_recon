802ca0fe875898baa7efa97c4f90fa94590d9a51
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-11-30 11:08:05 +0100

    add a script to combine all images in a slab

---
+++ comb_slice.py
@@ -0,0 +1,47 @@
+import argparse
+import h5py
+import os
+import pathlib
+
+import numpy as np
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+# %%
+if __name__ == "__main__":
+
+    # %% parse
+    parser = argparse.ArgumentParser(description='run dce reconstruction.')
+
+    parser.add_argument('--data',
+                        default='GeneBreast_CCCTrio#F636203.h5',
+                        help='radial k-space data')
+
+    parser.add_argument('--images_per_slab', type=int, default=192,
+                        help='total number of images per slab [default: 192]')
+
+    args = parser.parse_args()
+
+    OUT_DIR = DIR + '/' + args.data
+    OUT_DIR = OUT_DIR.split('.h5')[0]
+
+    assert os.path.exists(OUT_DIR), ['The directory ' + OUT_DIR + ' does not exist.']
+
+    # %%
+    acq_slices = []
+
+    for s in range(args.images_per_slab):
+
+        print('> slice ' + str(s).zfill(3))
+
+        fstr = OUT_DIR + '/reco_slice' + str(s).zfill(3)
+
+        f = h5py.File(fstr + '.h5', 'r')
+        acq_slices.append(f['temptv'][:])
+        f.close()
+
+    acq_slices = np.array(acq_slices)
+    print('> acq_slices shape: ', acq_slices.shape)
+
+    f = h5py.File(OUT_DIR + '/reco.h5', 'w')
+    f.create_dataset('temptv', data=acq_slices)
+    f.close()
