1369aac924359029e5323bfe4aa805f0f3644992
Zhengguo Tan <zhengguo.tan@gmail.com>
2024-01-18 18:34:05 +0100

    loop data takes DIR as the first argument

--- loop_data.sh
+++ loop_data.sh
@@ -1,20 +1,33 @@
 #/bin/bin -l
 
-FILES=$(find * -type f -name "*CCCTrio*.h5")
+if [ -z "$1" ]; then
+    echo "Find raw .h5 files in the current directory: $(pwd)"
+    DIR=$(pwd)
+else
+    echo "Find raw .h5 files in: $1"
+    DIR="$1"
+fi
+
+FILES=$(find "${DIR}" -maxdepth 1 -type f -name "*CCCTrio*.h5" -exec basename {} \;)
 
 echo "> files to be reconstructed: "
 echo "${FILES}"
 
-if [ -z "$1" ]; then
-    echo "SPOKES set as default: 72"
+if [ -z "${FILES}" ]; then
+    echo "no raw .h5 file found. exit."
+    exit
+fi
+
+if [ -z "$2" ]; then
+    echo "> SPOKES set as default: 72"
     SPOKES=72
 else
     NUM='^[0-9]+$'
 
-    if [[ $1 =~ $NUM ]]; then
-        SPOKES="$1"
+    if [[ $2 =~ $NUM ]]; then
+        SPOKES="$2"
     else
-        echo "Input $1 is not a number. SPOKES set as default: 72"
+        echo "> Input $2 is not a number. SPOKES set as default: 72"
         SPOKES=72
     fi
 fi
@@ -25,7 +38,7 @@ for F in ${FILES}; do
     echo "> reconstruct now ${F}"
 
     # reconstruct slice by slice
-    python dce_recon.py --data ${F} --spokes_per_frame ${SPOKES} --slice_idx 0 --slice_inc 192
+    python dce_recon.py --dir ${DIR} --data ${F} --spokes_per_frame ${SPOKES} --slice_idx 0 --slice_inc 192
 
     # combine all slices as a 4D matrix into one .h5 file
     python comb_slice.py --data ${F}
